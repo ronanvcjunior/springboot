ba65b7157944659c85263279b7e34a5bbeedc679
Ronan Vieira <ronanvcjunior@gmail.com>
2021-09-12 04:18:49 -0300

    Grafana

--- src/main/java/br/com/ronan/springboot/SpringbootApplication.java
+++ src/main/java/br/com/ronan/springboot/SpringbootApplication.java
@@ -1,7 +1,12 @@
 package br.com.ronan.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class SpringbootApplication {
@@ -10,4 +15,8 @@ public class SpringbootApplication {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
